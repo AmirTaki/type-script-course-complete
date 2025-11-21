ab6041ac06a6e8c8547ced025be0694fc793a4a5
amir <amirtaki623@gmail.com>
2025-11-22 03:29:50 +0330

    Object creation in TypeScript

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('Class construction in TypeScript');
+console.log('Object creation in TypeScript');
 class Account {
     id;
     name;
@@ -17,5 +17,11 @@ class Account {
         this.balance += amount;
     }
 }
-Account;
+const account = new Account(1, 'amir', 0);
+console.log(account);
+console.log(account.name);
+account.deposite(100);
+console.log(account.balance);
+console.log(typeof account);
+console.log(account instanceof Account);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-console.log('Class construction in TypeScript')
-
-
-// An object is a product of a class.
-
-// In object-oriented programming, we have a concept called class, which allows us to create as many objects as we want using a class.
+console.log('Object creation in TypeScript')
 
 class Account {
     id: number ;
@@ -27,7 +22,16 @@ class Account {
     }
 }
 
-Account
+const account =  new Account(1, 'amir', 0);
 
+console.log(account)
 
-// object === instance
+console.log(account.name)
+
+account.deposite(100)
+
+console.log(account.balance)
+
+console.log(typeof account)
+
+console.log(account instanceof Account)
