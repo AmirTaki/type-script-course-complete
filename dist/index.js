b0b0ed796f4fb36bc57d9796bbc3bb2d0efba145
amir <amirtaki623@gmail.com>
2025-11-22 00:33:24 +0330

    Class construction in TypeScript

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log(' Object-Orientation in TypeScript');
+console.log('Class construction in TypeScript');
+class Account {
+    id;
+    name;
+    balance;
+    constructor(id, name, balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+    deposite(amount) {
+        if (amount <= 0) {
+            throw new Error('Invalid amount');
+        }
+        this.balance += amount;
+    }
+}
+Account;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,33 @@
-console.log(' Object-Orientation in TypeScript')
+console.log('Class construction in TypeScript')
 
 
-/*
-Programming patterns
+// An object is a product of a class.
 
-1- Procedur
-2- functional 
-3- Event-Driven
-4- aspect oriented
-5- object oriented
+// In object-oriented programming, we have a concept called class, which allows us to create as many objects as we want using a class.
 
+class Account {
+    id: number ;
+    name: string;
+    balance: number;
 
-javaScript & typeScript => functional & object oriented 
-*/
+    // property
+    constructor(id: number, name: string, balance: number){
+        this.id = id;
+        this.name = name;
+        this.balance = balance
+    }
+    // method 
+    deposite(amount: number): void {
+        if(amount <= 0)
+        {
+            throw new Error('Invalid amount')
+        }
+       
+        this.balance += amount;
+    }
+}
 
-
-/* By putting objects together, our application is built. */
-
-/* Each object is a unit that consists of two basic parts: 1- Data (state) 2- Behavior  (operation) */
-
-/*
-Object-oriented philosophy: We can simulate concepts and entities that exist in the real world.
-*/
-
-/*
-person : 1- data ->( name, email)=> property
-         2- behavior ->   (talk() , sleep() )
-
- 
-In the programming world, the behavior of a function is called a method.
-And the data part in the variable is called a property.
-*/
-
-/* Each object in the program has a task, and all objects work together to achieve a specific goal. */
+Account
 
 
-/*
-Difference between object-oriented and functional programming"
-
-In object-oriented programming, the building blocks are objects.
-
-In functional programming, the units that make up those functions are
-
-
-*/
+// object === instance
