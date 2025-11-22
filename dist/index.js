ac1e06b20553fde1c7fdd77b7850bebbad82adb8
amir <amirtaki623@gmail.com>
2025-11-22 22:24:28 +0330

    Generic  in TypeScript

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('Abstraction in TypeScript');
-class Shape {
-    color;
-    constructor(color) {
-        this.color = color;
+console.log('Generic  in TypeScript');
+class KeyValuePair {
+    key;
+    value;
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
     }
 }
-class Circle extends Shape {
-    radius;
-    constructor(radius, color) {
-        super(color);
-        this.radius = radius;
-    }
-    render() {
-        console.log('Rendering a circle ...');
-    }
-}
-let circle = new Circle(2, 'gray');
-circle.render();
+let pair = new KeyValuePair(1, 'Apple');
+pair;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,11 @@
-console.log('protected Modifire in TypeScript')
-
-// access modifire 
-/*
-1- public 
-2- private
-3- protected
+console.log('Generic  in TypeScript')
 
 
-Private members cannot be inherited, but protected members can be inherited.
-A protected member is accessible from within the class and inheritance, but not outside the class.
-
-important:
-We use protected members when we need them.
-*/
-
-class Person {
-    constructor(
-        public fistName:string,
-        public lastName:string 
-    ){}
-
-    protected walk(){
-        console.log('Walking')
-    }
-
-    get fullName (): string {
-        return this.fistName + " " + this.lastName;
-    }
-
-}
-
-class Student extends Person {
-    constructor(
-            public studentId: number, 
-            firstName: string,  // // not access no modifre becuse Inhertance
-            lastName: string   // not access no modifre becuse Inhertance
-    ){
-        super(firstName, lastName)
-    }
-
-    takeTest() {
-        this.walk()
-        console.log('taking a test')
-    }
-}
-
-class Teacher extends Person {
-    override get fullName(): string {
-        // return "Professor " + this.fistName +" "  +this.lastName
-        return "Professor " + super.fullName
-    }
-}
-
-class Principal extends Person {
-    override get fullName() :string {
-        return "Principal " + super.fullName
-    }
+class KeyValuePair{
+    constructor(public key: number, public value:string){}
 }
 
 
-function printNames (people: Person[]): void{
-    for(let person of people){
-        console.log(person.fullName)
-    }
-}
+let pair =  new KeyValuePair(1, 'Apple')
+pair
 
-Student;
-
-Teacher;
-
-printNames([
-    new Student(1, 'amir', 'taki'),
-    new Teacher('mani', 'saniz'),
-    new Student(2, 'raha', 'porian'),
-    new Principal('shayan', 'proina')
-])
