94b881f5a43a7bea1a7804dd0374fbf01fadd05d
amir <amirtaki623@gmail.com>
2025-11-23 20:11:07 +0330

    class decorator in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-console.log(' decorator in TypeScript')
+console.log('class decorator in TypeScript')
+ 
+//  Prototype:  Every object in JavaScript has a prototype from which it inherits its properties and methods.
+function Component (constructor: Function):  void{
+    console.log('Component decorator called');
+    constructor.prototype.uniqueId = Date.now()
 
-// Descriptors are attributes that we give to classes and members, thus changing their behavior.
-// A decorator is actually a function that is called by the JavaScript runtime.
+    constructor.prototype.insertInDom = () => {
+        console .log('Inserting the component in the DOM')
+    }
+
+}
 
 @Component
 class ProfileComponent {
 
 }
+new ProfileComponent();
+
+
+// Whether we have created an instance or not, the destructor is called.
+
+// The declarator that we apply to a class is actually a function that our class calls as a declarator.
