d3e998507152e5ac280f582f153b5807c5e0bf91
amir <amirtaki623@gmail.com>
2025-11-24 03:48:00 +0330

    import & export in type script

---
+++ dist/exportModule/shape.d.ts
@@ -0,0 +1,9 @@
+export declare class Circle {
+    radius: number;
+    constructor(radius: number);
+}
+export declare class Square {
+    radius: number;
+    constructor(radius: number);
+}
+//# sourceMappingURL=shape.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,9 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('parameter decorator in type script');
-const watchedParameters = [];
-function Watch(target, methodName, parmeterIndex) {
-    watchedParameters.push({
-        methodName,
-        parmeterIndex
-    });
-    target;
-}
-class vehicle {
-    move(speed) { speed; }
-}
-__decorate([
-    __param(0, Watch)
-], vehicle.prototype, "move", null);
-new vehicle();
-console.log(watchedParameters);
+const shape = require("./exportModule/shape");
+console.log('import & export in type script');
+const circle = new shape.Circle(12);
+const square = new shape.Square(10);
+console.log(circle);
+console.log(square);
 //# sourceMappingURL=index.js.map
